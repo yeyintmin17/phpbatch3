e0d3868818ba4b051dac2ae7fca3a1370f81cb2e
bloneeee <blone5128@gmial.com>
2023-04-14 22:20:48 +0630

    l21 db methods & l22 creating db > 14 Apr 2023

--- part1/l21databasemethods.php
+++ part1/l21databasemethods.php
@@ -1,4 +1,6 @@
 <?php
+    // ini_set('display-errors', 1);
+
     // (i) MySQLi Procedural * normal
     // (ii) MySQLi Object-Oriented * normal
     // (iii) Using PDO (PHP Data Object) *** more secure 
@@ -13,4 +15,87 @@
     }
 
     echo "Connect Sucessfully";
+    echo "<hr/>";
+
+    $dbhost = 'localhost';
+    $dbuser = 'root';
+    $dbpass = '';
+    $conn = mysqli_connect($dbhost, $dbuser, $dbpass);
+
+    if(!$conn){
+        // echo "Failed to connect :". mysqli_connect_error();
+        // exit();
+
+        die("Failed to connect :". mysqli_connect_error());
+    }
+
+    echo "Connect Sucessfully";
+    echo "<hr/>";
+
+    // (ii) MySQLi Object-Oriented * normal
+
+    $dbhost = 'localhost';
+    $dbuser = 'root';
+    $dbpass = '';
+    $conn = new mysqli($dbhost, $dbuser, $dbpass);
+
+    if(mysqli_connect_error()){
+        die("Failed to connect :". mysqli_connect_error());
+    }
+
+    echo "Connect Sucessfully";
+    echo "<hr/>";
+
+    $conn = new mysqli('localhost', 'root', '', 'wdf6797');
+
+    if($conn->connect_errno){
+        echo "Failed to connect :". $conn->connect_errno;
+        exit();
+    }
+
+    echo "Connect Sucessfully <br/>";
+
+    $conn->query("SELECT * FROM buyers");
+    echo $conn->affected_rows;
+    $conn->close();
+
+    echo "<hr/>";
+
+    function openconn(){
+        $dbhost = 'localhost';
+        $dbuser = 'root';
+        $dbpass = '';
+        $dbconnect = new mysqli($dbhost, $dbuser, $dbpass);
+
+        return $dbconnect;
+    }
+
+    function closeconn($conn){
+        $conn->close();
+    }
+
+    $conn = openconn();
+    echo "Connect Sucessfully";
+    closeconn($conn);
+
+    echo "<hr/>";
+
+    // (iii) Using PDO (PHP Data Object) *** more secure 
+
+    $dbhost = 'localhost';
+    $dbuser = 'root';
+    $dbpass = '';
+    $dbname = 'wdf6797';
+
+    try{
+        // $conn = new PDO("mysqli:host=$dbhost", $dbuser, $dbpass);
+        $conn = new PDO("mysql:host=$dbhost;dbname=$dbname", $dbuser, $dbpass);
+
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        echo "Connect Sucessfully";
+    }catch(PDOException $e){
+        echo "Connect Failed : ". $e->getMessage();
+    }
+
+    echo "<hr/>";
 ?>
---
+++ part1/l22createdatabase.php
@@ -0,0 +1,75 @@
+<?php
+    ini_set('display_errors', 1);
+    
+    // => MySQLi Procedural
+
+    $conn = mysqli_connect("localhost", "root", "");
+    if(mysqli_connect_error()){
+        echo "Failed to Connect : ". mysqli_connect_error();
+        exit();
+    }
+
+    echo "Connect Sucessfully <br/>";
+
+    // Create Database
+
+    $sql = "CREATE DATABASE IF NOT EXISTS phpdbone";
+
+    if(mysqli_query($conn, $sql)){ // true or false
+        echo "Database Created Successfully";
+    }else{
+        echo "Error Found". mysqli_error($conn);
+    }
+
+    mysqli_close($conn); // after ct db
+    echo "<hr/>";
+
+    // => MySQLi Object-Oriented
+
+    $dbhost = 'localhost';
+    $dbuser = 'root';
+    $dbpass = '';
+
+    $conn = new mysqli($dbhost, $dbuser, $dbpass);
+
+    if($conn->connect_error){
+        // echo "Connection Failed : ". $conn->connect_errno;
+        // exit();
+
+        // die("Connection Failed : ". $conn->connect_errno);
+        die("Connection Failed : ". $conn->connect_error);
+    }
+
+    echo "Connected Sucessfully <br/>";
+
+    // Create Database
+
+    $sql = "CREATE DATABASE IF NOT EXISTS phpdbtwo";
+
+    if($conn->query($sql) === TRUE){
+        echo "Database Created Successfully";
+    }else{
+        echo "Error Found : ". $conn->error;
+    }
+
+    $conn->close();
+
+    echo "<hr/>";
+
+    // => POD
+
+    $dbhost = 'localhost';
+    $dbuser = 'root';
+    $dbpass = '';
+
+    try{
+        $conn = new PDO("mysql:host=$dbhost", $dbuser, $dbpass);
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); // catch err
+        $sql = "CREATE DATABASE IF NOT EXISTS phpdbthree";
+
+        $conn->exec($sql);
+        echo "Database Created Successfully";
+    }catch(PDOException $e){
+        echo "Error Found : ". $e->getMessage();
+    }
+?>
