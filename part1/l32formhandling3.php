c5d06796a05f1009fd24410eaa785cd8f8bd4e15
yeyintmin17 <blone5128@gmial.com>
2023-04-28 17:31:42 +0630

    l31, l32 form handling > 28 Apr 2023

--- part1/l32formhandling3.php
+++ part1/l32formhandling3.php
@@ -1,13 +1,45 @@
 <?php
+    ini_set('display_errors', 0);
+
     $name = $email = $password = $city = $favcolor = $bio = "";
+    $nameerr = $emailerr = $passerr = $cityerr = $favcolorerr = $bioerr = "";
     
     if($_SERVER["REQUEST_METHOD"] === "POST"){
-        $name = textfilter($_POST["name"]);
-        $email = textfilter($_POST["email"]);
-        $password = textfilter($_POST["password"]);
-        $city = textfilter($_POST["city"]);
-        $favcolor = textfilter($_POST["favcolor"]);
-        $bio = textfilter($_POST["bio"]);
+        if(empty($_POST["name"])){
+            $nameerr = "Name is required";
+        }else{
+            $name = textfilter($_POST["name"]);
+        }
+
+        if(empty($_POST["email"])){
+            $emailerr = "Email is required";
+        }else{
+            $email = textfilter($_POST["email"]);
+        }
+
+        if(empty($_POST["password"])){
+            $passerr = "Password is required";
+        }else{
+            $password = textfilter($_POST["password"]);
+        }
+        
+        if(empty($_POST["city"])){
+            $cityerr = "City is required";
+        }else{
+            $city = textfilter($_POST["city"]);
+        }
+
+        if(empty($_POST["favcolor"])){
+            $favcolorerr = "Fav color is required";
+        }else{
+            $favcolor = textfilter($_POST["favcolor"]);
+        }
+        
+        if(empty($_POST["bio"])){
+            $bioerr = "Bio is required";
+        }else{
+            $bio = textfilter($_POST["bio"]);
+        }
     }
 
     function textfilter($data){
@@ -20,33 +52,44 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>l31 Form Handling 2</title>
+        <title>l32 Form Handling 3</title>
+        <style type="text/css">
+            .error{
+                color: red;
+
+            }
+        </style>
     </head>
     <body>
         <!-- POST -->
         <form action="<?= $_SERVER['PHP_SELF']; ?>" method="POST">
             <div>
                 <label for="name">Name</label>
+                <span class="error">* <?php echo $nameerr; ?></span>
                 <input type="text" name="name" id="name" class="" placeholder="Enter Name..."/>
             </div>
 
             <div>
                 <label for="email">Email</label>
+                <span class="error">* <?php echo $emailerr; ?></span>
                 <input type="text" name="email" id="email" class="" placeholder="Enter Email..."/>
             </div>
 
             <div>
                 <label for="password">Password</label>
+                <span class="error">* <?php echo $passerr; ?></span>
                 <input type="password" name="password" id="password" class="" placeholder="Enter Password..."/>
             </div>
 
             <div>
                 <label for="city">City</label>
+                <span class="error">* <?php echo $cityerr; ?></span>
                 <input type="text" name="city" id="city" class="" placeholder="Enter City..."/>
             </div>
 
             <div>
                 <label>Fav Color</label>
+                <span class="error">* <?php echo $favcolorerr; ?></span>
 
                 <input type="radio" name="favcolor" id="favrd" class="" value="Red Color"/>
                 <label for="favrd">Red</label>
@@ -60,6 +103,7 @@
 
             <div>
                 <label for="bio">Bio</label>
+                <span class="error">* <?php echo $bioerr; ?></span>
                 <textarea name="bio" id="bio" class="" rows="5" cols="50"></textarea>
             </div>
 
